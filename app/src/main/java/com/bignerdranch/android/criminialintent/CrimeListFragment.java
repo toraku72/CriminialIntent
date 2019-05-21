fe2d780fc6f37f0ddb58d2769f3f4fbf9af2f056
toraku72 <tora.contact.ch@gmail.com>
2019-05-21 10:38:43 +0700

    Completed all challenges.

--- app/src/main/java/com/bignerdranch/android/criminialintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminialintent/CrimeListFragment.java
@@ -38,10 +38,10 @@ public class CrimeListFragment extends Fragment {
     }
 
     private void updateUI() {
-        CrimeLab crimeLab = CrimeLab.get(getActivity());
-        List<Crime> crimes = crimeLab.getCrimes();
-
         if (mAdapter == null) {
+            CrimeLab crimeLab = CrimeLab.get(getActivity());
+            List<Crime> crimes = crimeLab.getCrimes();
+
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
